8cbb89ab9134675cbe52068bd45429774fb43b1b
Chih-Ting Lin <chihtinglin03@gmail.com>
2019-03-07 20:31:10 +0800

    deploy website

---
+++ build/precache-manifest.1d7221d7c6c1e5c9003215fcce2df88e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b60d69382460e3280ba1",
+    "url": "/react-desserts-website/static/css/main.e4aa31c3.chunk.css"
+  },
+  {
+    "revision": "b60d69382460e3280ba1",
+    "url": "/react-desserts-website/static/js/main.b60d6938.chunk.js"
+  },
+  {
+    "revision": "b20b2152a05a18664700",
+    "url": "/react-desserts-website/static/js/runtime~main.b20b2152.js"
+  },
+  {
+    "revision": "15bb3ff8ca4cf3ed6100",
+    "url": "/react-desserts-website/static/js/2.15bb3ff8.chunk.js"
+  },
+  {
+    "revision": "8bdfeead812e40f29381c8c349c0de60",
+    "url": "/react-desserts-website/static/media/shopping-cart.8bdfeead.svg"
+  },
+  {
+    "revision": "19ab3b22ac5a5cfb50d5befd3d855e55",
+    "url": "/react-desserts-website/index.html"
+  }
+];
